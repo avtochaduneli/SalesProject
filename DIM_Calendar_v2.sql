8ecb91441493ed1b11025b4e275d2ba9c0c54a2b
avtochaduneli <107919539+avtochaduneli@users.noreply.github.com>
2023-12-01 16:55:59 +0400

    Add files via upload

---
+++ DIM_Calendar_v2.sql
@@ -0,0 +1,25 @@
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] as Date --,[DayNumberOfWeek]
+  , 
+  [EnglishDayNameOfWeek] as Day --,[SpanishDayNameOfWeek]
+  --,[FrenchDayNameOfWeek]
+  --,[DayNumberOfMonth]
+  --,[DayNumberOfYear]
+  , 
+  [WeekNumberOfYear] as WeekNo, 
+  [EnglishMonthName] as Month, 
+  LEFT(EnglishMonthName, 3) AS MonthShort --,[SpanishMonthName]
+  --,[FrenchMonthName]
+  , 
+  [MonthNumberOfYear] as MonthNo, 
+  [CalendarQuarter] as Quarter, 
+  [CalendarYear] as Year --,[CalendarSemester]
+  --,[FiscalQuarter]
+  --,[FiscalYear]
+  --,[FiscalSemester]
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimDate] 
+where 
+  CalendarYear >= 2019
+
